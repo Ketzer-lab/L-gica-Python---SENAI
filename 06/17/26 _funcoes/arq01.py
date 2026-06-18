be9169949ce8d06fbaa5ba69a08fc62497122f08
Ketzer-lab <guilherme.fketzer@gmail.com>
2026-06-17 22:15:50 -0300

    update1

--- 06/17/26 _funcoes/arq01.py
+++ 06/17/26 _funcoes/arq01.py
@@ -1,5 +1,7 @@
-def func1(parametro1, parametro2):
-    """Docstring: """
+def somar_valores(parametro1, parametro2):
+    """Docstring: Soma 2 valores. """
     #Corpo da função:
     resultado = parametro1 + parametro2
     return  resultado
+soma = somar_valores(2, 3)
+print(soma)
