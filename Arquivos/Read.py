4966def5b29092b18e1fc2e2bf26fb7a4fcfa04a
joaquimarend-hash <guilherme.fketzer@gmail.com>
2026-07-08 20:46:31 -0300

    Arq

---
+++ Arquivos/Read.py
@@ -0,0 +1,23 @@
+with open("exemplo.txt", "r") as f:
+    conteudo = f.read()
+    print(conteudo)
+
+print()
+
+with open ("exemplo.txt", "r") as f:
+    linha1 = f.readline()
+    linha2 = f.readline()
+    print(linha1)
+
+print()
+
+with open("exemplo.txt", "r") as f:
+    linhas = f.readlines()
+    for linha in linhas:
+        print(linha.strip())
+
+print()
+
+with open("exemplo.txt", "r") as f:
+    for linha in f:
+        print(linha)
