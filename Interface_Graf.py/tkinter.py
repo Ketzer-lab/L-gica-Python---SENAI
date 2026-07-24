44e0ad7b3986f05b69add64f8b303af8e483bed2
SkyLine-Sausen <guilherme.fketzer@gmail.com>
2026-07-24 20:42:06 -0300

    Tkinter_upd

--- Interface_Graf.py/tkinter.py
+++ Interface_Graf.py/tkinter.py
@@ -1,11 +1,12 @@
 import tkinter as tk
 
 root = tk.Tk()  #Cria a janela principal
+root.title("SENAI - Desenvolvimento de Sistemas")   #O título da janela
+
+root.geometry("600x400+100+100")    #Dita o tamanho da janela
 
 message = tk.Label(root, text="Hello, World!")  #Cria um rótulo como o texto "Hello, World!"
 
 message.pack()  #Posicionao rótulo na janela
 
 root.mainloop()     #Inicia o loop principal da interface grádica
-
-# Aqui não funciona por causa de alguma configuração da máquina
---
+++ Interface_Graf.py/tkinter_size2.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+root.geometry("400x300")
+
+root.resizable(False, False)    #Impede que a janela seja ampliada manualmente com o cursor
+
+root.minsize(300, 200)          #Tamanho minimo da janela
+root.maxsize(800, 600)          #Tamanho maximo da janela
+
+root.mainloop()
