c6280842b236641aabffc839c603836d325095e5
Ketzer-lab <guilherme.fketzer@gmail.com>
2026-07-31 19:40:13 -0300

    UPD

--- Interface_Graf.py/Atividade/IMC.py
+++ Interface_Graf.py/Atividade/IMC.py
@@ -1,16 +1,16 @@
 import tkinter as tk
 from tkinter import ttk
 
-root = tk.Tk()
-root.title("SENAI - Desenvolvimento de Sistemas")
-root.geometry("800x600")
+root = tk.Tk()  # A janela principal, a raiz o root.
+root.title("SENAI - Desenvolvimento de Sistemas")   # Título que estará acima da janela
+root.geometry("800x600")    # Tamanho da janela
 
 def calculo():
-    try:
-        peso = float(entrypeso.get())
-        altura = float(entryaltura.get())
+    try:    # Try é usado quando o código pode dar um erro, então após ele é usado um except.
+        peso = float(entrypeso.get())   # O .get pegao que foi digitado em uma caixa entry e envia para o sistema.
+        altura = float(entryaltura.get())   # Nesse caso o .get envia os números para que o IMC possa ser calculado.
 
-        imc = peso / (altura*altura)
+        imc = peso / (altura*altura)    # Não se pode digitar palavras pois elas resultariam num erro no cálculo.
 
         if imc < 18.5:
             classificacao = "Abaixo do peso"
@@ -21,17 +21,20 @@ def calculo():
         else:
             classificacao = "Obesidade"
 
-        resultado.config(
-            text=f"Seu IMC é: {imc:.2f}\nClassificação: {classificacao}"
+        resultado.config(   # O .config é usado para alterar a proptiedade de um widget, nesse caso ele muda o label resultado que era "" para "Seu IMC é: {imc:.2f}\nClassificação: {classificacao}".
+            text=f"Seu IMC é: {imc:.2f}\nClassificação: {classificacao}"    # \n passa o resto do texto para linha de baixo.
+        )
+        labelinstrucao.config(
+            text=""
         )
 
-    except ValueError:
+    except ValueError:  # Except é usado quando o código da erro, então ele comanda o sistema fazer outra coisa ao invés de ser interrompido, nesse caso impimir uma mensagem.
         resultado.config(text="Digite valores válidos!")
 
 
-labelpeso = tk.Label(root, text="Peso(Kg)")
-labelpeso.pack()
-entrypeso = tk.Entry(root)
+labelpeso = tk.Label(root, text="Peso(Kg)")     #tk.Label são as palavras que aparecerão na interface gráfica.
+labelpeso.pack()    # .pack() manda a variavel para a interface
+entrypeso = tk.Entry(root)  # Entry é usado no Tkinter para que o usuario possa digitar uma linha de texto
 entrypeso.pack()
 
 labelaltura = tk.Label(root, text="Altura(m)")
@@ -39,12 +42,15 @@ labelaltura.pack()
 entryaltura = tk.Entry(root)
 entryaltura.pack()
 
-button = tk.Button(            
-    root,
-    text="Calcular",
-    command=calculo
+button = tk.Button(     #tk.Button cria um botão na interface que faz alguma coisa quando clicado.    
+    root,               # É onde o botão está, nesse caso a janela principal, a raiz o root.
+    text="Calcular",    # O texto que estará em cima do botão
+    command=calculo     # O comando que ele vai fazer
     )
 button.pack()
+
+labelinstrucao = tk.Label(root, text="Preencha os campos e clique em calcular.")
+labelinstrucao.pack()
 
 resultado = tk.Label(root, text="")
 resultado.pack()
