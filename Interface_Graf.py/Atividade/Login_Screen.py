ebb0782f97ff54d7ccf6855301157ceb488072c6
Ketzer-lab <guilherme.fketzer@gmail.com>
2026-08-05 19:18:07 -0300

    Color_UPD

--- Interface_Graf.py/Atividade/Login_Screen.py
+++ Interface_Graf.py/Atividade/Login_Screen.py
@@ -6,36 +6,42 @@ root.config(bg="white")
 root.geometry("400x600")
 
 label_login = tk.Label(root, text="Faça seu login")
+label_login.config(bg="white")
 label_login.pack()
 
 imagem = tk.PhotoImage(file="profile.png")
 label_imagem = tk.Label(root, image=imagem)
 label_imagem.image = imagem
+label_imagem.config(bg="white")
 label_imagem.pack()
 
 label_usuario = tk.Label(root, text="Usuário:")
+label_usuario.config(bg="white")
 label_usuario.pack(anchor="w")
 entry_usuario = tk.Entry(root)
 entry_usuario.pack()
 
 label_senha = tk.Label(root, text="Senha:")
+label_senha.config(bg="white")
 label_senha.pack(anchor="w")
 entry_senha = tk.Entry(root)
 entry_senha.pack()
 
 botao = tk.Button(
     root,
-    text="      Entrar      "
+    text="      Entrar      ",
+    bg="white"
 )
 botao.pack()
 
 caixinha = tk.Checkbutton(
     root,
-    text="Lembrar-me"
+    text="Lembrar-me",
+    bg="white"
     )
 caixinha.pack(side="left")
 
-esqueceu = tk.Label(root, text="Esqueceu sua senha?")
+esqueceu = tk.Label(root, text="Esqueceu sua senha?", bg="white")
 esqueceu.pack(side="right")
 
 root.mainloop()
